2e4edcf515fdd0de2d69d7fb32af37cc8f0474da
JoeJack404 <artshekh@gmail.com>
2022-01-15 22:01:48 +0300

    Реализация Test

--- StudyTest/StudyTest/Program.cs
+++ StudyTest/StudyTest/Program.cs
@@ -7,6 +7,7 @@ namespace StudyTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("StudyTest");
         }
     }
 }
