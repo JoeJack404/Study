6e41b5694a05d4fd8c869175a86884896f9fc0b8
JoeJack404 <artshekh@gmail.com>
2022-01-22 23:19:27 +0300

    Добавлен HashTable

---
+++ HashTable/HashTable/Hashtable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class Hashtable
+    {
+        private string[] Table = new string[100];
+
+        private int HashFunction(string value)
+        {
+            return 1;
+        }
+
+
+    }
+}
