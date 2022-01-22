596d986d1f161a6f4dc321df76dfa2b81763669f
JoeJack404 <artshekh@gmail.com>
2022-01-22 23:51:30 +0300

    Метод Add

--- HashTable/HashTable/Hashtable.cs
+++ HashTable/HashTable/Hashtable.cs
@@ -10,11 +10,30 @@ namespace HashTable
     {
         private string[] Table = new string[100];
 
-        private int HashFunction(string value)
+        private int HashFunction(string value, int sizeTable = 100)
         {
             return 1;
         }
 
-
+        public bool Add(string value)
+        {
+            int position = HashFunction(value);
+            if (Table[position] == null)
+            {
+                Table[position] = value;
+                return true;
+            }
+            else
+            {
+                //Array.Resize(ref Table, Table.Length + 100);
+                //foreach (string oldValue in Table)
+                //{
+                //    int newPosition = HashFunction(oldValue, Table.Length + 100);
+                //    Table[newPosition] = oldValue;
+                //}
+                //Add(value);
+                return false;
+            }
+        }
     }
 }
