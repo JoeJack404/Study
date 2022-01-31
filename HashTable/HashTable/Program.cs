e096900bfccb6831588932284b7db505727f05f9
JoeJack404 <artshekh@gmail.com>
2022-01-31 23:39:01 +0300

    Попытки в делегаты

--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -43,6 +43,18 @@ namespace HashTable
             {
                 Console.WriteLine("Записи нет");
             }
+            int SumString(string value, int size)
+            {
+                size = 100;
+                int hashResult = 0;
+                for (int i = 0; i < value.Length; i++)
+                {
+                    char symbol = value[i];
+                    hashResult = Convert.ToInt32(symbol) + hashResult;
+                }
+                return hashResult % size;
+            }
+            hashtable.ChangeHashFunction(SumString);
         }
     }
 }
