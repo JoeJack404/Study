413045c013a6f18422f2b74fb21c24af090352cc
JoeJack404 <artshekh@gmail.com>
2022-02-12 15:12:55 +0300

    Binary Tree

---
+++ BinaryTree/BinaryTree/BinaryTree.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class BinaryTree
+    {
+        private Knot root;
+
+        public void TryAddKnot(int data)
+        {
+            if (root == null)
+            {
+                root.Data = data;
+                root.Left = null;
+                root.Right = null;
+            }
+            else if (root.Data > data)
+            {
+                root = root.Left;
+                TryAddKnot(data);
+            }
+            else
+            {
+                root = root.Right;
+                TryAddKnot(data);
+            }
+        }
+
+        public bool IsContain(int data)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+            else if (root.Data == data)
+            {
+                return true;
+            }
+            else if (root.Data > data)
+            {
+                root = root.Left;
+                IsContain(data);
+                return true;
+            }
+            else
+            {
+                root = root.Right;
+                IsContain(data);
+                return true;
+            }
+        }
+    }
+}
---
+++ BinaryTree/BinaryTree/Knot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Knot
+    { 
+        public int Data { get; set; }
+        public Knot Left { get; set; }
+        public Knot Right { get; set; }
+    }
+}
