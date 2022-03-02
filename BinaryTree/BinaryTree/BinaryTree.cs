5c616f4404fdcbc3aa694a23077499b690f57357
JoeJack404 <artshekh@gmail.com>
2022-03-02 19:02:29 +0300

    Попытки в Remove

--- BinaryTree/BinaryTree/BinaryTree.cs
+++ BinaryTree/BinaryTree/BinaryTree.cs
@@ -19,7 +19,35 @@ namespace BinaryTree
             }
             else
             {
-                root.Add(root, data);
+                AddKnot(root, data);
+            }
+        }
+
+        private void AddKnot(Knot knot, int data)
+        {
+            if (knot.Data > data)
+            {
+                if (knot.Left == null)
+                {
+                    knot.Left = new Knot();
+                    knot.Left.Data = data;
+                }
+                else
+                {
+                    AddKnot(knot.Left, data);
+                }
+            }
+            else
+            {
+                if (knot.Right == null)
+                {
+                    knot.Right = new Knot();
+                    knot.Right.Data = data;
+                }
+                else
+                {
+                    AddKnot(knot.Right, data);
+                }
             }
         }
 
@@ -31,7 +59,69 @@ namespace BinaryTree
             }
             else
             {
-                return root.IsContain(root, data);
+                return IsContain(root, data);
+            }
+        }
+
+        private bool IsContain(Knot knot, int data)
+        {
+            if (knot == null)
+            {
+                return false;
+            }
+            else if (knot.Data == data)
+            {
+                return true;
+            }
+            else if (knot.Data > data)
+            {
+                return IsContain(knot.Left, data);
+            }
+            else
+            {
+                return IsContain(knot.Right, data);
+            }
+        }
+
+        public bool RemoveKnot(int data)
+        {
+            if (!IsContain(root, data))
+            {
+                return IsContain(root, data);
+            }
+            else
+            {
+                return ;
+            }
+        }
+
+        private bool RemoveKnot(Knot knot, int data)
+        {
+            if (knot == null)
+            {
+                return false;
+            }
+            else if (knot.Left == null & knot.Right == null)
+            {
+                knot = null;
+                return true;
+            }
+            else if (knot.Left == null ^ knot.Right == null)
+            {
+                if (knot.Left == null)
+                {
+                    knot = knot.Right;
+                    return true;
+                }
+                else
+                {
+                    knot = knot.Right;
+                    return true;
+                }
+            }
+            else
+            {
+
             }
         }
     }
--- BinaryTree/BinaryTree/Knot.cs
+++ BinaryTree/BinaryTree/Knot.cs
@@ -11,53 +11,5 @@ namespace BinaryTree
         public int Data { get; set; }
         public Knot Left { get; set; }
         public Knot Right { get; set; }
-
-        public void Add(Knot knot, int data)
-        {
-            if (knot.Data > data)
-            {
-                if (knot.Left == null)
-                {
-                    knot.Left = new Knot();
-                    knot.Left.Data = data;
-                }
-                else
-                {
-                    Add(knot.Left, data);
-                }
-            }
-            else
-            {
-                if (knot.Right == null)
-                {
-                    knot.Right = new Knot();
-                    knot.Right.Data = data;
-                }
-                else
-                {
-                    Add(knot.Right, data);
-                }
-            }
-        }
-
-        public bool IsContain(Knot knot, int data)
-        {
-            if (knot == null)
-            {
-                return false;
-            }
-            else if (knot.Data == data)
-            {
-                return true;
-            }
-            else if (knot.Data > data)
-            {
-                return IsContain(knot.Left, data);
-            }
-            else
-            {
-                return IsContain(knot.Right, data);
-            }
-        }
     }
 }
